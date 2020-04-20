0d21716fd227f259342f1c6ad8d05fe3b7271670
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-04-20 23:53:42 +0200

    Added user table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('123456'),
+            'type' => 'admin',
+        ]);
+    }
+}
